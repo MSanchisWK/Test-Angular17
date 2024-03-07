db853eed99507219e11d86d9cb789c884a73af2c
MSanchisWK <marti.sanchis-roman@whitekongsoftware.com>
2024-03-07 21:17:17 +0100

    wip(songs): development of song component

--- src/app/pages/songs/songs.component.ts
+++ src/app/pages/songs/songs.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AppService } from '../../services/app.service'; // Replace 'path-to-your-song-service' with the actual path to your SongService
+import { Artist } from 'src/app/interfaces/Artist';
+import { Song } from 'src/app/interfaces/Song';
 
 @Component({
   selector: 'app-songs',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./songs.component.css']
 })
 export class SongsComponent {
+  songs: Song[] = [];
+  artists: Artist[] = [];
+  genres: string[] = ['Rock', 'Pop', 'Jazz', 'Metal', 'Blues', 'Rap', 'Country', 'Classical', 'Electronic', 'Reggae'];
 
+  constructor(private songService: AppService) {}
+
+  ngOnInit() {
+    this.loadData();
+  }
+
+  loadData() {
+    this.songService.getSongs().subscribe(songs => {
+      this.songs = songs;
+    });
+
+    this.songService.getArtists().subscribe(artists => {
+      this.artists = artists;
+    });
+  }
+
+  getArtistNameById(id: number): string {
+    const artist =  this.artists.find(artist => artist.id === id);
+    if(!artist){
+      return 'Unknown'
+    }
+    return artist.name 
+  }
 }
